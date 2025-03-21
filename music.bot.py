da17cb3087e07142f5fa714441637dadfe216c1b
이하민 <luna07080906@gmail.com>
2025-03-22 01:23:05 +0900

    first message

--- music.bot.py
+++ music.bot.py
@@ -6,7 +6,7 @@ import yt_dlp as youtube_dl
 import os
 #from dico_token import Token  # dico_token.py에서 Token 임포트
 
-Token = os.getemv('DISCORD_TOKEN')
+Token = os.environ.get('DISCORD_TOKEN')
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
 
